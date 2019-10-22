e595500869bafeadb74dd1af1a35e5ded7750f8e
vittorio96 <vittoriomaria.denti@mail.polimi.it>
2019-10-22 16:18:42 +0200

    merge results from k-fold cross-validation

--- main.py
+++ main.py
@@ -38,21 +38,37 @@ def main():
     accuracy_list = []
     precision_list = []
     f1_score_list = []
+    n_folds = 2
 
-    k_fold_cross_validator = StratifiedKFold(n_splits=2)
+    k_fold_cross_validator = StratifiedKFold(n_splits=n_folds)
+    epoch = 0
 
-    for train_index, val_index in k_fold_cross_validator.split(X_train, Y_train):
-        for k in range(1, 3):
-            k_list.append(k)
+    for k in range(1, 3):
+
+        accuracy_sum = 0
+        precision_sum = 0
+        f1_score_sum = 0
+
+        for train_index, val_index in k_fold_cross_validator.split(X_train, Y_train):
+
             x_train, x_val = X_train[train_index], X_train[val_index]
             y_train, y_val = Y_train[train_index], Y_train[val_index]
             prediction = knn.fit_knn_model(k, x_train, x_val, y_train)  # y_train is used to know the class to vote
             ## Compute metrics
-            accuracy_list.append(accuracy_score(y_val, prediction))
-            precision_list.append(precision_score(y_val, prediction, average='macro'))
-            f1_score_list.append(f1_score(y_val, prediction, average='macro'))
+            accuracy_sum += accuracy_score(y_val, prediction)
+            precision_sum += precision_score(y_val, prediction, average='macro')
+            f1_score_sum += f1_score(y_val, prediction, average='macro')
 
-    ##Save results on CSV file
+        ## Epoch completed, hyperparameters evaluated, compute average values over the folds
+        k_list.append(k)
+        accuracy_list.append(round(accuracy_sum/n_folds, 5))
+        precision_list.append(round(precision_sum/n_folds, 5))
+        f1_score_list.append(round(f1_score_sum/n_folds, 5))
+
+        print("Epoch "+str(epoch)+" finished")
+        epoch += 1
+
+    ##Save final results on CSV file
 
     df = pd.DataFrame({'K': k_list, 'Accuracy': accuracy_list, 'Precision': precision_list, 'F1 score': f1_score_list})
     df.to_csv("tuning/tuning_"+dataset_name+".csv")
